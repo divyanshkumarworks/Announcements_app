5645ea6397c4c981fd7ff9d1e5c40ca3982a8a6e
rishu <divyanshkumarworks@gmail.com>
2023-08-03 15:24:23 +0530

    added single and multiple query handler

--- src/pages/api/company.ts
+++ src/pages/api/company.ts
@@ -2,21 +2,30 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../../libs/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  var query = req.query.query;
-  console.log(query)
-  query = query.map((value) => parseInt(value as string))
-  console.log(typeof(query))
+  var scrip_cd = req.query.scrip_cd;
+
+  const { db } = await connectToDatabase();
+  const collection = db.collection('company_announcements');
   try {
-    const { db } = await connectToDatabase();
-    const collection = db.collection('company_announcements');
+    if (typeof(scrip_cd) === "object" ) {
+    
+      scrip_cd = scrip_cd.map((value) => parseInt(value as string))
 
+      const announcements = await collection
+        .find({ SCRIP_CD: { $in : scrip_cd } })
+        .toArray();
 
-    const announcements = await collection
-      .find({ SCRIP_CD: { $in : query } })
-      .toArray();
+      res.status(200).json(announcements);
+    }
+    else {
+      const announcements = await collection
+        .find({ SCRIP_CD: parseInt(scrip_cd as string) })
+        .toArray();
 
-    res.status(200).json(announcements);
-  } catch (error) {
+      res.status(200).json(announcements);
+    } 
+  }
+  catch (error) {
     res.status(500).json({ message: 'Error fetching data from the database' });
   }
 }
