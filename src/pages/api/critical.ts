5d98892b0ee1bc2e191154632463b9fe6b54c578
rishu <divyanshkumarworks@gmail.com>
2023-08-03 23:17:24 +0530

    optimized some logic

--- src/pages/api/critical.ts
+++ src/pages/api/critical.ts
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(200).json(announcements);
 	}
   	catch (error) {
-	    res.status(500).json({ error: 'Internal Server Error' });
- 	}
+    console.error('Error fetching news:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
--- src/pages/api/special_period.ts
+++ src/pages/api/special_period.ts
@@ -20,46 +20,44 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const collection = db.collection('company_announcements');
 
 
-    if (!start_date || !end_date) {
-      return res.status(400).json({ error: 'start_date and end_date are required.' });
+    if (!start_date && !end_date) {
+      if (!scrip_cd) {
+        return res.status(400).json({ error: 'Invalid array provided in query parameters.' });
+      }
+      else{
+        if (!isStringArray(scrip_cd)) {
+          const announcements = await collection
+          .find({ SCRIP_CD: parseInt(scrip_cd as string) })
+          .toArray();
+
+          res.status(200).json(announcements);
+        }
+        else {
+          const convertedArray = scrip_cd.map((value) => Number(value));
+
+          const announcements = await collection
+          .find({ SCRIP_CD: { $in : convertedArray } })
+          .toArray();
+
+          res.status(200).json(announcements);
+        }
+      }
     }
-
-    const new_start_date = convertToDate(start_date);
-    const new_end_date = convertToDate(end_date);
-
-    if (new_start_date === null || new_end_date === null) {
-      return res.status(400).json({ error: 'Invalid date format provided in query parameters.' });
-    }
-
 
     if (start_date && end_date) {
+
+      const new_start_date = convertToDate(start_date);
+      const new_end_date = convertToDate(end_date);
+
+      if (new_start_date === null || new_end_date === null) {
+        return res.status(400).json({ error: 'Invalid date format provided in query parameters.' });
+      }
 
       const announcements = await collection
       .find({ "DT_TM": { "$gte" : new_start_date , "$lte": new_end_date} })
       .toArray();
 
       res.status(200).json(announcements);
-    }
-    if (scrip_cd) {
-      if (!scrip_cd || !isStringArray(scrip_cd)) {
-        return res.status(400).json({ error: 'Invalid array provided in query parameters.' });
-      }
-      if (typeof(scrip_cd) !== "string"){
-
-        const convertedArray = scrip_cd.map((value) => Number(value));
-
-        const announcements = await collection
-        .find({ SCRIP_CD: { $in : convertedArray } })
-        .toArray();
-
-        res.status(200).json(announcements);
-      }else if (typeof(scrip_cd) === "string") {
-        const announcements = await collection
-        .find({ SCRIP_CD: parseInt(scrip_cd as string) })
-        .toArray();
-
-        res.status(200).json(announcements);
-      }
     }
   } catch (error: any) {
     console.error('Error occurred:', error);
